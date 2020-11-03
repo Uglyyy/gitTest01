f0a964130a6ad50b8561edac8d9775a46087a2e6
Uglyyy <515749176@qq.com>
2020-11-03 22:06:49 +0800

    first commit

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,26 @@
+package pojo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Liubg
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
